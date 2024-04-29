02db7bae9a18c8c9d11d878e7edb4007976314f9
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-04-29 15:19:28 +0500

    Added fallback values for ClickHouse client

--- database/db.ts
+++ database/db.ts
@@ -1,13 +1,13 @@
 import { createClient } from '@clickhouse/client'
 
 const db = createClient({
-	url: Bun.env.CLICKHOUSE_URL,
-	username: Bun.env.CLICKHOUSE_USERNAME,
-	password: Bun.env.CLICKHOUSE_PASSWORD,
-	database: Bun.env.CLICKHOUSE_NAME,
+	host: Bun.env.DB_HOST || 'http://localhost:8123',
+	username: Bun.env.DB_USERNAME || 'default',
+	password: Bun.env.DB_PASSWORD,
+	database: Bun.env.DB_NAME || 'default',
 	compression: { request: true, response: true },
 	request_timeout: 0,
-	max_open_connections: 2000,
+	max_open_connections: 2000
 })
 
 export default db
