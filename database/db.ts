c3a47b1b4ab8742a1623f6126a563b7f165f1fba
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-05-01 10:59:50 +0500

    Fixed Database connection problem

--- database/db.ts
+++ database/db.ts
@@ -1,12 +1,11 @@
 import { createClient } from '@clickhouse/client'
 
 const db = createClient({
-	host: Bun.env.DB_HOST || 'http://localhost:8123',
+	url: Bun.env.DB_URL || 'http://localhost:8123',
 	username: Bun.env.DB_USERNAME || 'default',
 	password: Bun.env.DB_PASSWORD,
 	database: Bun.env.DB_NAME || 'default',
 	compression: { request: true, response: true },
-	request_timeout: 50,
 	max_open_connections: 2000
 })
 
