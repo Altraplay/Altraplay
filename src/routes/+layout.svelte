5a96090d3f8d2ed01eeaa086361f5eb28ba8c501
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-05-15 16:46:29 +0500

    Added sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import '../style.css'
 	import Header from '@Components/Header.svelte'
+	import Sidebar from '@Components/Sidebar.svelte'
 </script>
 
 <Header />
+<Sidebar />
 <slot></slot>
