7b5f79a8bd8d55368e0c50dcdfc4f767851367b4
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-05-23 14:48:56 +0500

    Specialized values of level and role in user types

--- src/types/user.ts
+++ src/types/user.ts
@@ -8,8 +8,35 @@ export interface User {
 	following: string
 	profile_picture: string
 	banner: string
-	level: string
-	role: string
+	level:
+		| 'The Cosmic Lion'
+		| 'King of Content'
+		| 'Neon Ninja'
+		| 'Electric Knight'
+		| 'The Blazing Beacon'
+		| 'Dashing Diamond'
+		| 'Legion of Content'
+		| 'Silent Soul'
+		| 'Neon Dragon'
+		| 'The Blazing Souls of Creators'
+		| 'Cyber Soul'
+		| 'Mutant Rangers '
+		| 'The Content Wizard'
+		| 'The Majestic Dou'
+		| 'The Majestic Trio '
+		| 'The Majestic Squad '
+		| 'Technology Demon'
+		| 'Gaming Dragon'
+	role:
+		| 'Super Admin'
+		| 'Basic Admin'
+		| 'Moderator'
+		| 'Premium User'
+		| 'Diamond User'
+		| 'User'
+		| 'Admin'
+		| 'Member'
+		| 'Advertiser'
 	points: number
 	needs: number
 	links: string[]
