c507d9adfcd449bd9276521e4b9fda341ed7a0cd
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-06-01 16:33:23 +0500

    Fixed db config

--- database/db.ts
+++ database/db.ts
@@ -3,12 +3,13 @@ import cassandra from 'cassandra-driver'
 export const client = new cassandra.Client({
 	contactPoints: Bun.env.SCYLLADB_CONTACTPOINTS?.split(','),
 	localDataCenter: Bun.env.SCYLLADB_LOCALDATACENTER,
-	credentials: { username: Bun.env.SCYLLADB_USERNAME!, password: Bun.env.SCYLLADB_PASSWORD! }
+	credentials: { username: Bun.env.SCYLLADB_USERNAME!, password: Bun.env.SCYLLADB_PASSWORD! },
+	keyspace: 'tg'
 })
 
 export async function executeQuery(query: string, params: any[] = []) {
 	try {
-		const result = await client.execute(query, params, { prepare: true, keyspace: 'techgunner' })
+		const result = await client.execute(query, params, { prepare: true })
 		return result
 	} catch (error) {
 		console.error('Query execution error:', error)
