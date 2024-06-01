1cfe43672b6b281ee6d7788d547348309ee7b2b3
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-06-01 15:11:32 +0500

    Initialized ScyllaDB client, yes everyone we're migrating to ScyllaDB, so I asked ClickHouse support team if it would be a good choice as a primary DB, long story short they said no use it for analytics and logs, longest commit msg I've ever written

---
+++ database/db.ts
@@ -0,0 +1,17 @@
+import cassandra from 'cassandra-driver'
+
+export const client = new cassandra.Client({
+	contactPoints: Bun.env.SCYLLADB_CONTACTPOINTS?.split(','),
+	localDataCenter: Bun.env.SCYLLADB_LOCALDATACENTER,
+	credentials: { username: Bun.env.SCYLLADB_USERNAME!, password: Bun.env.SCYLLADB_PASSWORD! }
+})
+
+export async function executeQuery(query: string, params: any[] = []) {
+	try {
+		const result = await client.execute(query, params, { prepare: true, keyspace: 'techgunner' })
+		return result
+	} catch (error) {
+		console.error('Query execution error:', error)
+		throw error
+	}
+}
