308bc85ec86e28d630e912eef3526ae2ccf03428
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-06-09 17:57:13 +0500

    Removed unnecessary tables from our clickhouse schema and added a new table for analytics

--- database/clickhuseSchema.ts
+++ database/clickhuseSchema.ts
@@ -1,206 +1,46 @@
-import db from './db'
-
-const users = async () => {
-	try {
-		await db.command({
-			query: `CREATE TABLE IF NOT EXISTS users (
-        username String,
-        name String,
-        bio String DEFAULT '',
-        email String,
-        password String,
-        followers Int256 DEFAULT 0,
-        following String DEFAULT '{}',
-        profile_picture String DEFAULT 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png',
-        banner String DEFAULT '',
-        level String DEFAULT 'Silent Soul',
-        role String DEFAULT 'user',
-        points Int256 DEFAULT 0,
-        needs Int256 DEFAULT 100,
-        links Array(String),
-        verified Bool DEFAULT false,
-        skills Array(String) DEFAULT [''],
-        language Array(String) DEFAULT [''],
-        team Array(String) DEFAULT [''],
-        notifications String DEFAULT '{}',
-        is_history_on Bool DEFAULT true,
-        liked Array(String) DEFAULT [''],
-        disliked Array(String) DEFAULT [''],
-        only_visible_to String DEFAULT 'everyone',
-        is_email_verified Bool DEFAULT false,
-        verification_token String DEFAULT '',
-        earning String DEFAULT '{}',
-        achievements String DEFAULT '{}',
-        joined DateTime64 DEFAULT now()
-      ) ENGINE MergeTree()
-      ORDER BY (username, name)
-      PRIMARY KEY username`
-		})
-	} catch (e) {
-		console.error(e)
-	}
-}
-
-async function blogs() {
-	try {
-		await db.command({
-			query: `CREATE TABLE IF NOT EXISTS blogs (
-                id String,
-                title String,
-                author String,
-                content String,
-                cover String,
-                likes Int256 DEFAULT 0,
-                dislikes Int256 DEFAULT 0,
-                views Int256 DEFAULT 0,
-                comments String DEFAULT '{}',
-                tags Array(String),
-                images Array(String) DEFAULT [''],
-                visible_to Array(String) DEFAULT ['everyone'],
-                categories Array(String),
-                published_at DateTime64 DEFAULT now()
-            ) ENGINE MergeTree()
-            ORDER BY (id, author)
-            PRIMARY KEY id`
-		})
-	} catch (e) {
-		console.error(e)
-	}
-}
-
-async function videos() {
-	try {
-		await db.command({
-			query: `CREATE TABLE IF NOT EXISTS videos (
-                id String,
-                title String,
-                url String,
-                creator String,
-                description String,
-                cover String,
-                likes Int256 DEFAULT 0,
-                dislikes Int256 DEFAULT 0,
-                views Int256 DEFAULT 0,
-                comments String DEFAULT '{}',
-                tags Array(String),
-                visible_to Array(String) DEFAULT ['everyone'],
-                categories Array(String),
-                published_at DateTime64 DEFAULT now()
-            ) ENGINE MergeTree()
-            ORDER BY (id, creator)
-            PRIMARY KEY id`
-		})
-	} catch (e) {
-		console.error(e)
-	}
-}
-
-async function posts() {
-	try {
-		await db.command({
-			query: `CREATE TABLE IF NOT EXISTS posts (
-                id String,
-                title String,
-                url String,
-                owner String,
-                likes Int256 DEFAULT 0,
-                dislikes Int256 DEFAULT 0,
-                views Int256 DEFAULT 0,
-                comments String DEFAULT '{}',
-                tags Array(String),
-                visible_to Array(String) DEFAULT ['everyone'],
-                categories Array(String),
-                published_at DateTime64 DEFAULT now()
-            ) ENGINE MergeTree()
-            ORDER BY (id, owner)
-            PRIMARY KEY id`
-		})
-	} catch (e) {
-		console.error(e)
-	}
-}
-
-async function messages() {
-	try {
-		await db.command({
-			query: `CREATE TABLE IF NOT EXISTS messages (
-                id String,
-                sender String,
-                receiver String,
-                message String,
-                read Bool DEFAULT false,
-                reply_of String DEFAULT '',
-                edited Bool DEFAULT false,
-                sent_at DateTime64 DEFAULT now()
-            ) ENGINE MergeTree()
-            ORDER BY (id, sender)
-            PRIMARY KEY id`
-		})
-	} catch (e) {
-		console.error(e)
-	}
-}
-
-async function history() {
-	try {
-		await db.command({
-			query: `CREATE TABLE IF NOT EXISTS history (
-                id String,
-                user String,
-                visit_url String,
-                type String,
-                time DateTime64 DEFAULT now()
-            ) ENGINE MergeTree()
-            ORDER BY (id, user)
-            PRIMARY KEY id`
-		})
-	} catch (e) {
-		console.error(e)
-	}
-}
-
-async function searchHistory() {
-	try {
-		await db.command({
-			query: `CREATE TABLE IF NOT EXISTS search_history (
-                id String,
-                user String,
-                query String,
-                time DateTime64 DEFAULT now()
-            ) ENGINE MergeTree()
-            ORDER BY (id, user)
-            PRIMARY KEY id`
-		})
-	} catch (e) {
-		console.error(e)
-	}
-}
+import db from './clickhouse'
 
 async function logs() {
 	try {
 		await db.command({
 			query: `CREATE TABLE IF NOT EXISTS logs (
-                id String,
+                type String,
                 log String,
                 time DateTime64 DEFAULT now()
             ) ENGINE MergeTree()
-            ORDER BY (id, log)
-            PRIMARY KEY id`
+            ORDER BY (type, time)
+            PRIMARY KEY type`
 		})
 	} catch (e) {
-		console.error(e)
+		console.error(`Error creating table for logs, error: ${e}`)
+	}
+}
+
+async function analytics() {
+	try {
+		await db.command({
+			query: `CREATE TABLE IF NOT EXISTS analytics (
+            responseTime String,
+            responseCode String,
+            cpuUsage String,
+            memoryUsage String,
+            requestNumber String,
+            path String,
+            time DateTime64 DEFAULT now()
+            ) ENGINE MergeTree()
+             ORDER BY (requestNumber, responseTime, responseCode, path, time)
+             PRIMARY KEY requestNumber`
+		})
+	} catch (e) {
+		console.error(`Error creating table for analytics, error: ${e}`)
 	}
 }
 
 async function push() {
-	await users()
-	await blogs()
-	await videos()
-	await posts()
-	await messages()
-	await history()
-	await searchHistory()
 	await logs()
+	await analytics()
+	console.log('Tables created')
+	process.exit(0)
 }
 
 push()
