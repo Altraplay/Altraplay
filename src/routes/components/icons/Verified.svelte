fa3c3dca360620b3512cb3806ec3643590264a7a
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-06-25 16:21:10 +0500

    Added verify icon component

---
+++ src/routes/components/icons/Verified.svelte
@@ -0,0 +1,18 @@
+<script>
+   export let width = 15
+   export let height = 15
+   export let className = ''
+</script>
+<svg
+	fill="none"
+	{height}
+	{width}
+	viewBox="0 0 24 24"
+	xmlns="http://www.w3.org/2000/svg"
+	class="{className}">
+	<path
+		clip-rule="evenodd"
+		d="M1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM11.2071 16.2071L18.2071 9.20711L16.7929 7.79289L10.5 14.0858L7.20711 10.7929L5.79289 12.2071L9.79289 16.2071C9.98043 16.3946 10.2348 16.5 10.5 16.5C10.7652 16.5 11.0196 16.3946 11.2071 16.2071Z"
+		fill-rule="evenodd"
+		class="fill-primary" />
+</svg>
