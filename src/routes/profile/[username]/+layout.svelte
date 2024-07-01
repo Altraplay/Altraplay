b3f4771335da321683fc81aae669944813baca55
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-07-01 12:57:30 +0500

    Improved navigation tabs and adjusted layout a bit - profile page

--- src/routes/profile/[username]/+layout.svelte
+++ src/routes/profile/[username]/+layout.svelte
@@ -16,44 +16,49 @@
 		><img src={data.banner} alt="banner" class="h-full w-full object-cover blur-xl" /></div>
 	<div class="z-30 m-5 h-[28vh] w-[95vw] self-center"
 		><img src={data.banner} alt="banner" class="h-full w-full object-cover" /></div>
-	<Profile {data} className="ml-12 mt-6" />
-	<div
-		class="-mt-[58rem] flex h-12 w-[37rem] translate-x-[360px] items-center gap-3 bg-highlight-1 p-3"
-		><a
-			href={$page.url.pathname}
-			class="rounded-[5px] bg-highlight-2 px-3 {activeTab === $page.url.pathname
-				? 'bg-primary font-bold text-background'
-				: ''}">Home</a>
-		<a
-			href="{$page.url.pathname}/blog"
-			class="rounded-[5px] bg-highlight-2 px-3 {activeTab === `${$page.url.pathname}/blog`
-				? 'bg-primary font-bold text-background'
-				: ''}">Blogs</a>
-		<a
-			href="{$page.url.pathname}/vidieo"
-			class="rounded-[5px] bg-highlight-2 px-3 {activeTab === `${$page.url.pathname}/vidieo`
-				? 'bg-primary font-bold text-background'
-				: ''}">Videos</a>
-		<a
-			href="{$page.url.pathname}/post"
-			class="rounded-[5px] bg-highlight-2 px-3 {activeTab === `${$page.url.pathname}/post`
-				? 'bg-primary font-bold text-background'
-				: ''}">Posts</a>
-		<a
-			href="{$page.url.pathname}/collaction"
-			class="rounded-[5px] bg-highlight-2 px-3 {activeTab === `${$page.url.pathname}/collaction`
-				? 'bg-primary font-bold text-background'
-				: ''}">Collections</a>
-		<a
-			href="{$page.url.pathname}/service"
-			class="rounded-[5px] bg-highlight-2 px-3 {activeTab === `${$page.url.pathname}/service`
-				? 'bg-primary font-bold text-background'
-				: ''}">Services</a>
-		<a
-			href="{$page.url.pathname}/about"
-			class="rounded-[5px] bg-highlight-2 px-3 {activeTab === `${$page.url.pathname}/about`
-				? 'bg-primary font-bold text-background'
-				: ''}">About</a>
-	</div>
-	<slot />
+	<section class="flex translate-x-[340px] flex-col">
+		<div class="p-3 *:px-5">
+			<a
+				href={$page.url.pathname}
+				class={activeTab === $page.url.pathname ? 'border-b-[3px] border-primary font-bold' : ''}
+				>Home</a>
+			<a
+				href="{$page.url.pathname}/blog"
+				class={activeTab === `${$page.url.pathname}/blog`
+					? 'border-b-[3px] border-primary font-bold'
+					: ''}>Blogs</a>
+			<a
+				href="{$page.url.pathname}/vidieo"
+				class={activeTab === `${$page.url.pathname}/vidieo`
+					? 'border-b-[3px] border-primary font-bold'
+					: ''}>Videos</a>
+			<a
+				href="{$page.url.pathname}/post"
+				class={activeTab === `${$page.url.pathname}/post`
+					? 'border-b-[3px] border-primary font-bold'
+					: ''}>Posts</a>
+			<a
+				href="{$page.url.pathname}/collaction"
+				class={activeTab === `${$page.url.pathname}/collaction`
+					? 'border-b-[3px] border-primary font-bold'
+					: ''}>Collections</a>
+			<a
+				href="{$page.url.pathname}/achivement"
+				class={activeTab === `${$page.url.pathname}/achivement`
+					? 'border-b-[3px] border-primary font-bold'
+					: ''}>Achievements</a>
+			<a
+				href="{$page.url.pathname}/service"
+				class={activeTab === `${$page.url.pathname}/service`
+					? 'border-b-[3px] border-primary font-bold'
+					: ''}>Services</a>
+			<a
+				href="{$page.url.pathname}/about"
+				class={activeTab === `${$page.url.pathname}/about`
+					? 'border-b-[3px] border-primary font-bold'
+					: ''}>About</a>
+		</div>
+		<slot />
+	</section>
+	<Profile {data} className="translate-y-[39vh] translate-x-10 absolute" />
 </main>
