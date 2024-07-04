c934243426a2e3439b2a1bee685e3ed96fbf5312
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-07-04 13:33:16 +0500

    Custom classes to the img component

--- src/routes/components/Img.svelte
+++ src/routes/components/Img.svelte
@@ -4,9 +4,10 @@
 	export let height: string
 	export let alt: string
 	export let glowEffect = true
+	export let className = ''
 </script>
 
-<div style="height: {height}; width: {width};">
+<div style="height: {height}; width: {width};" class="{className}">
 	{#if glowEffect}
 		<img {src} {alt} style="height: {height}; width: {width}" class="absolute blur-lg" />
 	{/if}
