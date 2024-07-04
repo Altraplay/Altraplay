95dbdf737dc7b3a0dee2f6315c6b794612503cfe
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-07-04 12:35:01 +0500

    Improved Sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,13 @@
 	import Header from '@Components/Header.svelte'
 	import Sidebar from '@Components/Sidebar.svelte'
 	import Cursor from '@Components/Cursor.svelte'
+	let show = false
 </script>
 
 <Header />
-<Sidebar />
+<div class="flex">
+	<Sidebar bind:show />
+	<slot class="{show ? 'ml-[250px]' : 'ml-[50px]'}"/>
+</div>
 <Cursor />
-<slot></slot>
+
--- src/routes/components/Sidebar.svelte
+++ src/routes/components/Sidebar.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { onMount } from 'svelte'
 	import { page } from '$app/stores'
-	import { slide, fade } from 'svelte/transition'
+	import { slide } from 'svelte/transition'
 
-	let show = false
+	export let show = false
 
 	function toggleMenu() {
 		show = !show
@@ -22,93 +22,72 @@
 	})
 </script>
 
-{#if show}
-	<div class="bg" on:click={() => (show = false)} transition:fade />
-	<nav
-		class="fixed z-[125] flex h-[93vh] w-[255px] flex-col overflow-y-auto bg-background text-[1.2rem] [&>a]:ml-[5px] [&>a]:mt-[5px] [&>a]:rounded-[5px] [&>a]:px-[10px] [&>a]:py-[5px] [&>a]:transition-all [&>a]:duration-700"
-		transition:slide={{ axis: 'x', duration: 700 }}>
-		<a
-			class="hover:ml-[25px] hover:bg-primary hover:text-highlight-1 focus:ml-[25px] focus:bg-primary focus:text-highlight-1 {$page
-				.url.pathname === '/'
-				? '!ml-[25px] bg-primary text-highlight-1'
-				: ''}"
-			data-sveltekit-preload-data
-			href="/">
-			<b class="fa-solid fa-house-chimney absolute mt-1"></b>
-			<b class="ml-10">Home</b>
-		</a>
-		<a
-			class="hover:ml-[25px] hover:bg-primary hover:text-highlight-1 focus:ml-[25px] focus:bg-primary focus:text-highlight-1 {$page
-				.url.pathname === '/explore'
-				? '!ml-[25px] bg-primary text-highlight-1'
-				: ''}"
-			data-sveltekit-preload-data
-			href="/explore">
-			<b class="fa-solid fa-magnifying-glass absolute mt-1"></b>
-			<b class="ml-10">Explore</b>
-		</a>
-		<a
-			class="hover:ml-[25px] hover:bg-primary hover:text-highlight-1 focus:ml-[25px] focus:bg-primary focus:text-highlight-1 {$page
-				.url.pathname === '/inbox'
-				? '!ml-[25px] bg-primary text-highlight-1'
-				: ''}"
-			data-sveltekit-preload-data
-			href="/inbox">
-			<b class="fa-solid fa-envelope absolute mt-1"></b>
-			<b class="ml-10">Inbox</b>
-		</a>
-		<a
-			class="hover:ml-[25px] hover:bg-primary hover:text-highlight-1 focus:ml-[25px] focus:bg-primary focus:text-highlight-1 {$page
-				.url.pathname === '/blog'
-				? '!ml-[25px] bg-primary text-highlight-1'
-				: ''}"
-			data-sveltekit-preload-data
-			href="/blog">
-			<b class="fa-solid fa-pen absolute mt-1"></b>
-			<b class="ml-10">Blogs</b>
-		</a>
-		<a
-			class="hover:ml-[25px] hover:bg-primary hover:text-highlight-1 focus:ml-[25px] focus:bg-primary focus:text-highlight-1 {$page
-				.url.pathname === '/freelance'
-				? '!ml-[25px] bg-primary text-highlight-1'
-				: ''}"
-			data-sveltekit-preload-data
-			href="/freelance">
-			<b class="fa-solid fa-briefcase absolute mt-1"></b>
-			<b class="ml-10">Freelance</b>
-		</a>
-		<a
-			class="hover:ml-[25px] hover:bg-primary hover:text-highlight-1 focus:ml-[25px] focus:bg-primary focus:text-highlight-1 {$page
-				.url.pathname === '/tools'
-				? '!ml-[25px] bg-primary text-highlight-1'
-				: ''}"
-			data-sveltekit-preload-data
-			href="/tools">
-			<b class="fa-solid fa-wrench absolute mt-1"></b>
-			<b class="ml-10">Tools</b>
-		</a>
-		<a
-			class="hover:ml-[25px] hover:bg-primary hover:text-highlight-1 focus:ml-[25px] focus:bg-primary focus:text-highlight-1 {$page
-				.url.pathname === '/wallpapers'
-				? '!ml-[25px] bg-primary text-highlight-1'
-				: ''}"
-			data-sveltekit-preload-data
-			href="/wallpapers">
-			<b class="fa-solid fa-image absolute mt-1"></b>
-			<b class="ml-10">Wallpapers</b>
-		</a>
-	</nav>
-{/if}
-
-<style>
-	.bg {
-		width: 100%;
-		height: 100%;
-		z-index: 100;
-		position: fixed;
-		background: rgba(255, 255, 255, 0.03);
-		box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-		backdrop-filter: blur(1.8px);
-		-webkit-backdrop-filter: blur(1.8px);
-	}
-</style>
+<nav
+	class="sticky z-[125] flex h-[93.7vh] flex-col overflow-y-auto overflow-x-hidden bg-background text-[1.2rem] transition-all duration-700 [&>a]:rounded-[5px] [&>a]:transition-all [&>a]:duration-700 {show
+		? 'w-[240px] gap-[5px] [&>a:focus]:ml-[25px] [&>a:focus]:bg-primary [&>a:focus]:text-highlight-1 [&>a:hover]:ml-[25px] [&>a:hover]:bg-primary [&>a:hover]:text-highlight-1 [&>a]:w-[210px] [&>a]:px-[10px] [&>a]:py-[5px]'
+		: 'w-[50px] items-center gap-3 [&>a:focus]:bg-primary [&>a:focus]:text-highlight-1 [&>a:hover]:bg-primary [&>a:hover]:text-highlight-1 [&>a]:px-2 [&>a]:py-1'}"
+	transition:slide={{ axis: 'x', duration: 500 }}>
+	<a
+		class={$page.url.pathname === '/'
+			? `${show ? '!ml-[25px]' : ''} bg-primary text-highlight-1`
+			: ''}
+		data-sveltekit-preload-data
+		href="/">
+		<span class="fa-solid fa-house-chimney {show ? 'absolute mt-1' : ''}"></span>
+		<b class="ml-10{show ? '' : ' hidden'}">Home</b>
+	</a>
+	<a
+		class={$page.url.pathname === '/explore'
+			? `${show ? '!ml-[25px]' : ''} bg-primary text-highlight-1`
+			: ''}
+		data-sveltekit-preload-data
+		href="/explore">
+		<span class="fa-solid fa-magnifying-glass {show ? 'absolute mt-1' : ''}"></span>
+		<b class="ml-10{show ? '' : ' hidden'}">Explore</b>
+	</a>
+	<a
+		class={$page.url.pathname === '/inbox'
+			? `${show ? '!ml-[25px]' : ''} bg-primary text-highlight-1`
+			: ''}
+		data-sveltekit-preload-data
+		href="/inbox">
+		<span class="fa-solid fa-envelope {show ? 'absolute mt-1' : ''}"></span>
+		<b class="ml-10{show ? '' : ' hidden'}">Inbox</b>
+	</a>
+	<a
+		class={$page.url.pathname === '/blog'
+			? `${show ? '!ml-[25px]' : ''} bg-primary text-highlight-1`
+			: ''}
+		data-sveltekit-preload-data
+		href="/blog">
+		<span class="fa-solid fa-pen {show ? 'absolute mt-1' : ''}"></span>
+		<b class="ml-10{show ? '' : ' hidden'}">Blogs</b>
+	</a>
+	<a
+		class={$page.url.pathname === '/freelance'
+			? `${show ? '!ml-[25px]' : ''} bg-primary text-highlight-1`
+			: ''}
+		data-sveltekit-preload-data
+		href="/freelance">
+		<span class="fa-solid fa-briefcase {show ? 'absolute mt-1' : ''}"></span>
+		<b class="ml-10{show ? '' : ' hidden'}">Freelance</b>
+	</a>
+	<a
+		class={$page.url.pathname === '/tools'
+			? `${show ? '!ml-[25px]' : ''} bg-primary text-highlight-1`
+			: ''}
+		data-sveltekit-preload-data
+		href="/tools">
+		<span class="fa-solid fa-wrench {show ? 'absolute mt-1' : ''}"></span>
+		<b class="ml-10{show ? '' : ' hidden'}">Tools</b>
+	</a>
+	<a
+		class={$page.url.pathname === '/wallpapers'
+			? `${show ? '!ml-[25px]' : ''} bg-primary text-highlight-1`
+			: ''}
+		data-sveltekit-preload-data
+		href="/wallpapers">
+		<span class="fa-solid fa-image {show ? 'absolute mt-1' : ''}"></span>
+		<b class="ml-10{show ? '' : ' hidden'}">Wallpapers</b>
+	</a>
+</nav>
