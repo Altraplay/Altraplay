b9b7e35a2848c16a217a34cc4315f4d6f48d4180
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-07-05 19:46:18 +0500

    Added more options to the Img component

--- src/routes/components/Img.svelte
+++ src/routes/components/Img.svelte
@@ -1,15 +1,33 @@
 <script lang="ts">
 	export let src: string
-	export let width = 'auto'
-	export let height = 'auto'
+	export let width = ''
+	export let height = ''
+	export let minWidth = ''
+	export let minHeight = ''
+	export let maxWidth = ''
+	export let maxHeight = ''
 	export let alt: string
 	export let glowEffect = true
 	export let className = ''
 </script>
 
-<div style="height: {height}; width: {width};" class="{className}">
+<div
+	style="{height ? `height: ${height};` : ''} {width ? `width: ${width};` : ''} {maxWidth
+		? `max-width: ${maxWidth}`
+		: ''}; {minWidth ? `min-width: ${minWidth};` : ''} {maxHeight
+		? `max-hight: ${maxHeight};`
+		: ''} {minHeight ? `min-hight: ${minHeight};` : ''}"
+	class="flex items-center justify-center {className}">
 	{#if glowEffect}
-		<img {src} {alt} style="height: {height}; width: {width}" class="absolute blur-lg" />
+		<img
+			{src}
+			{alt}
+			style="{height ? `height: ${height};` : ''} {width ? `width: ${width};` : ''} {maxWidth
+				? `max-width: ${maxWidth};`
+				: ''} {minWidth ? `min-width: ${minWidth};` : ''} {maxHeight
+				? `max-hight: ${maxHeight};`
+				: ''} {minHeight ? `min-hight: ${minHeight};` : ''}"
+			class="absolute blur-lg" />
 	{/if}
-    <img {src} {alt} class="h-full w-full object-cover relative">
+	<img {src} {alt} class="relative h-full w-full object-cover" />
 </div>
