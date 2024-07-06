98396932d7c398f3778004f6b5f302626a353cd2
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-07-06 18:41:00 +0500

    Slight improvements to the card

--- src/routes/components/Card.svelte
+++ src/routes/components/Card.svelte
@@ -20,31 +20,32 @@
 	export let className = ''
 </script>
 
-<a
-	href="/{href}/{data.id}"
-	class="h-fit max-w-[311px] overflow-hidden {className}"
-	data-sveltekit-preload-data>
-	<Img src={data.cover} alt="Cover Image" width="310px" height="180px" />
-	<a href="/profile/{data.user.username}" class="mt-4 w-fit" data-sveltekit-preload-data>
-		<!-- svelte-ignore a11y-img-redundant-alt -->
-		<div class="size-10">
-			<img
-				src={data.user.profile_picture}
-				alt="profile picture"
-				class="mt-5 h-full w-full rounded-md" />
+<div class="h-fit max-w-[22rem] min-w-[20rem] overflow-hidden {className}">
+	<a
+		href="/{href}/{data.id}"
+		data-sveltekit-preload-data>
+		<Img src={data.cover} alt="Cover Image" minWidth="20rem" maxWidth="22rem" height="13rem" />
+		<a href="/profile/{data.user.username}" class="mt-4 w-fit" data-sveltekit-preload-data>
+			<!-- svelte-ignore a11y-img-redundant-alt -->
+			<div class="size-10">
+				<img
+					src={data.user.profile_picture}
+					alt="profile picture"
+					class="mt-5 h-full w-full rounded-md" />
+			</div>
+		</a>
+		<div class="-mt-11 ml-12">
+			<h5 class="line-clamp-2 text-[1.19rem]">{data.title}</h5>
+			<div class="flex">
+				<a href="/profile/{data.user.username}">
+					<h6 class="text-slate-400">{data.user.name}</h6>
+				</a>
+				{#if data.user.verified}
+					<Verified className="translate-x-1 translate-y-1" />
+				{/if}
+			</div>
+			<div class="text-slate-400"
+				>views: {abbreviateNumber(data.views)} • {formatTime(new Date(data.created_at))}</div>
 		</div>
 	</a>
-	<div class="-mt-11 ml-12">
-		<h5 class="line-clamp-2 text-[1.19rem]">{data.title}</h5>
-		<div class="flex">
-			<a href="/profile/{data.user.username}">
-				<h6 class="text-slate-400">{data.user.name}</h6>
-			</a>
-			{#if data.user.verified}
-				<Verified className="translate-x-1 translate-y-1" />
-			{/if}
-		</div>
-		<div class="text-slate-400"
-			>views: {abbreviateNumber(data.views)} • {formatTime(new Date(data.created_at))}</div>
-	</div>
-</a>
+</div>
