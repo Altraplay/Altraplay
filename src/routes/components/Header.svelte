2b3e1629924cdc9e5a033b430ed41079ee0ac087
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-07-07 20:09:53 +0500

    Replaced menu icon with our temporary TG logo since the sidebar is no longer expandable.

--- src/routes/components/Header.svelte
+++ src/routes/components/Header.svelte
@@ -22,19 +22,7 @@
 </script>
 
 <header class="sticky left-0 top-0 z-[150] flex w-screen items-center justify-evenly py-2 h-[58px] bg-background">
-	<button
-		id="menu"
-		class="hover:fill-white focus:fill-white"
-		aria-label="Toggle menu"
-		aria-keyshortcuts="Control+Space">
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			height="30px"
-			viewBox="0 -960 960 960"
-			width="30px"
-			class="fill-slate-400"
-			><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" /></svg>
-	</button>
+	<a href="/"><img src="/favicon.png" alt="Logo" width="28"></a>
 	<a href="/"><h3>Tech Gunner</h3></a>
 	<form
 		class="flex h-10 w-[78vw] items-center justify-center"
