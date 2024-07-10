466273428a38f5208bd56abbef6d690aedf2ec9f
Tech Gunner <164009551+TechGunners@users.noreply.github.com>
2024-07-10 20:00:58 +0500

    Get all comments route now also returns info about the user who commented

--- src/routes/api/Controllers/comment.ts
+++ src/routes/api/Controllers/comment.ts
@@ -3,20 +3,37 @@ import db from '@DB/orm'
 import pushLogs from '$lib/logs'
 import { serverErr } from '$lib/constant'
 
-const route = new Elysia()
-.get('/:type/:id/comments', async ({ params, query, error }) => {
-	try {
-		return await db.findMany({
-			table: 'comments',
-			where: { comment_on: `${params.type}/${params.id}` },
-			select: ['id', 'comment', 'author', 'time', 'likes', 'dislikes'],
-			orderBy: { likes: 'desc' },
-			limit: query?.limit || 10,
-		})
-	} catch (err) {
-        await pushLogs(`Error fetching comments for ${params.type}: ${params.id}, ${err}`);
-		error(500, serverErr)
+const route = new Elysia({ prefix: '/:type/:id/comment' }).get(
+	'/',
+	async ({ params, query, error }) => {
+		try {
+			const { comments } = await db.findMany({
+				table: 'comments',
+				where: { comment_on: `${params.type}/${params.id}` },
+				select: ['id', 'comment', 'author', 'time', 'likes', 'dislikes'],
+				orderBy: { likes: 'DESC' },
+				limit: query?.limit || 10
+			})
+
+			if (comments) {
+				const list = await Promise.all(
+					comments?.map(async comment => {
+						const author = await db.findUnique({
+							table: 'users',
+							where: { is_email_verified: true, id: comment.author },
+							select: ['username', 'name', 'profile_picture', 'verified']
+						})
+
+						return { ...comment, author }
+					})
+				)
+				return { comments: list }
+			}
+		} catch (err) {
+			await pushLogs(`Error fetching comments for ${params.type}: ${params.id}, ${err}`)
+			error(500, serverErr)
+		}
 	}
-})
+)
 
 export default route
